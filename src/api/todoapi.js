14cd6da96e6cbd1056a6ce2fae34f63f71bc8c4b
VaishaliNaresh <vaishalin@digisenz.com>
2026-05-04 12:25:51 +0530

    issue found and fix for baseurl

--- src/api/todoapi.js
+++ src/api/todoapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-     baseURL: " https://task-management-pro-dre1.onrender.com/api/",
+     baseURL: "https://task-management-pro-dre1.onrender.com/api/",
     // baseURL: "http://localhost:8090/api"
 });
 export const fetchTodo = () => API.get("/todos");
